754ff6c959822335281216e4c0fa0df58534689b
tomonari <zcsxcacg@i.softbank.jp>
2022-03-22 15:01:47 +0900

    課題提出用コミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
