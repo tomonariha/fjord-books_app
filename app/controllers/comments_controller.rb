c380c3f6c02390015ea588fe5099c8e420da2460
tomonari <zcsxcacg@i.softbank.jp>
2022-04-10 00:01:38 +0900

    共通するコメント部分のパーシャル化をした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def create
     comment = @commentable.comments.build(comment_params)
     comment.user_id = current_user.id
-    comment.save
+    comment.save!
     redirect_to request.referer
   end
 
